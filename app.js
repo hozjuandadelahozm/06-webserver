b6d99c790a027a278f2df687a9444540011c75ce
Juan David De la hoz <hozjuanda11@gmail.com>
2022-04-27 23:43:04 -0500

    inicio webserver

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+http.createServer( (req, res) => {
+
+    // res.writeHead(200, {'Content-Type': 'application/json' });
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200, {'Content-Type': 'application/csv' });
+
+    const persona = {
+        id: 1,
+        nombre: 'Juan David'
+    }
+
+    res.write( 'id, nombre\n' );
+    res.write( '1, Fernando\n' );
+    res.write( '2, Maria\n' );
+    res.write( '3, Juan\n' );
+    res.write( '4, Pedro\n' );
+    res.end();
+
+})
+.listen( 4000 );
+
+console.log('Escuchando el puerto', 4000);
+

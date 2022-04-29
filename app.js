ab0766719f35c1b00e02ee2b982c7e5f47dd9d3b
Juan David De la hoz <hozjuanda11@gmail.com>
2022-04-28 23:32:11 -0500

    primer adelanto webserver

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 4000;
 
-http.createServer( (req, res) => {
+//Servir contenido estático
+app.use( express.static( 'public' ) )
 
-    // res.writeHead(200, {'Content-Type': 'application/json' });
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
-    res.writeHead(200, {'Content-Type': 'application/csv' });
 
-    const persona = {
-        id: 1,
-        nombre: 'Juan David'
-    }
-
-    res.write( 'id, nombre\n' );
-    res.write( '1, Fernando\n' );
-    res.write( '2, Maria\n' );
-    res.write( '3, Juan\n' );
-    res.write( '4, Pedro\n' );
-    res.end();
-
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola Mundo en su respectiva ruta')
 })
-.listen( 4000 );
 
-console.log('Escuchando el puerto', 4000);
+app.get('/generic', (req, res) => {
+    res.sendFile( __dirname + '/public/generic.html')
+})
 
+app.get('/elements', (req, res) => {
+    res.sendFile( __dirname + '/public/elements.html')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/404.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
